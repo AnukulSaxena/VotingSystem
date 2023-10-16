fe7a1332c4de2eb36ac7375a601476f82db2e0c7
Anukul <copopoco@gmail.com>
2023-10-16 18:43:11 +0530

    All Functionality Done

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
-from flask import Flask, render_template, request
-from face import abc
+from flask import Flask, render_template, request, redirect, url_for
+import face_recognition
+import dlib
+import cv2
+from imutils.video import WebcamVideoStream
+import time
+import openpyxl
 
-
-# Create a Flask application instance
 app = Flask(__name__)
 
-# Function to check if input is in a file
-def check_input(input1, input2):
-    # Check input1 in aadhar.txt
+known_image = face_recognition.load_image_file("anukul.png")
+known_encoding = face_recognition.face_encodings(known_image)[0]
+
+def abc():
+    detect = dlib.get_frontal_face_detector()
+    vs = WebcamVideoStream(src=0).start()
+
+    start_time = time.time()
+    while True:
+        frame = vs.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        subjects = detect(gray, 0)
+        if subjects:
+            rgb_small_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+            face_names = []
+
+            for face_encoding in face_encodings:
+                matches = face_recognition.compare_faces([known_encoding], face_encoding)
+                if True in matches:
+                    vs.stop()
+                    return 1
+
+        # Check if a certain time limit (e.g., 30 seconds) has passed
+        if time.time() - start_time > 10:
+            vs.stop()
+            break
+
+    return 0
+
+@app.route('/process_data', methods=['POST'])
+def process_data():
+    input1 = request.form.get('input1')
+    input2 = request.form.get('input2')
+
     with open("aadhar.txt", "r") as file1:
         if input1 not in file1.read():
             return "Input 1 does not match in aadhar.txt"
@@ -17,20 +53,45 @@ def check_input(input1, input2):
         if input2 not in file2.read():
             return "Input 2 does not match in voter.txt"
 
-    # If both inputs are valid, call the 'abc' function from the 'face' module
-    abc()
+    result = abc()
 
-# Define the route to handle the form
-@app.route("/", methods=["GET", "POST"])
+    if result == 0:
+        return redirect(url_for("index"))
+    else:
+        return redirect(url_for("face_matched"))
+
+@app.route('/face_matched')
+def face_matched():
+    return render_template('vote.html')
+
+@app.route("/")
 def index():
-    result = None
+    return render_template("index.html")
 
-    if request.method == "POST":
-        input1 = request.form.get("input1")
-        input2 = request.form.get("input2")
-        result = check_input(input1, input2)
+@app.route("/input_vote", methods=['POST'])
+def input_vote():
+    selected_option = request.form.get('option')
 
-    return render_template("index.html", result=result)
+    # Load the existing Excel file or create a new one if it doesn't exist
+    try:
+        workbook = openpyxl.load_workbook('result.xlsx')
+    except FileNotFoundError:
+        workbook = openpyxl.Workbook()
+
+    # Select the first sheet in the workbook
+    sheet = workbook.active
+
+    # Find the next empty row in the sheet
+    next_row = len(sheet['A']) + 1
+
+    # Write the selected option to the Excel file
+    sheet[f'A{next_row}'] = selected_option
+
+    # Save the Excel file
+    file_name = 'result.xlsx'
+    workbook.save(file_name)
+    result = f"Result appended to {file_name}"
+    return result
 
 if __name__ == "__main__":
     app.run(debug=True)
